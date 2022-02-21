128b9bc991ee46e1cac2205a10ef6dc372459597
brian d foy <brian.d.foy@gmail.com>
2022-02-21 15:27:12 -0500

    Initial creation with bmt

---
+++ lib/Mynx.pm
@@ -0,0 +1,65 @@
+package Mynx;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Mynx - Undescribed module Mynx
+
+=head1 SYNOPSIS
+
+	use Mynx;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/mynx
+
+=head1 AUTHOR
+
+brian d foy, C<< <brian d foy> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2022, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
