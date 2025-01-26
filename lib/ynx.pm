a15f506a76eecf19120b6aa93f3360bbf8ceb6cd
brian d foy <briandfoy@pobox.com>
2025-01-26 14:00:43 -0500

    update email address

--- lib/ynx.pm
+++ lib/ynx.pm
@@ -61,11 +61,11 @@ This source is in Github:
 
 =head1 AUTHOR
 
-brian d foy, <bdfoy@cpan.org>
+brian d foy, <briandfoy@pobox.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2022, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2022, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
