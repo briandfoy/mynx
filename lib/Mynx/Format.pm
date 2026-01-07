eaa621ab6f895088c27cb2ef0248264627bda81b
brian d foy <briandfoy@pobox.com>
2026-01-07 13:52:32 -0500

    Normalize and update copyright

--- lib/Mynx/Format.pm
+++ lib/Mynx/Format.pm
@@ -57,7 +57,7 @@ brian d foy, <briandfoy@pobox.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2022, brian d foy <briandfoy@pobox.com>. All rights reserved.
+Copyright © 2022-2026, brian d foy <briandfoy@pobox.com>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
